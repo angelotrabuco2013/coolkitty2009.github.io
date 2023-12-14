d76320aeb67d3187cf0fface6a98ae398eae9762
angelotrabuco2013 <153839847+angelotrabuco2013@users.noreply.github.com>
2023-12-14 15:45:29 -0300

    Create globalExceptionHandler.js

---
+++ globalExceptionHandler.js
@@ -0,0 +1,19 @@
+// This file is part of scratch2online, which is a project that allows you to run Scratch 2.0 on your browser.
+
+window.onerror = function(msg, url, line, col, error) {
+   // Note that col & error are new to the HTML 5 spec and may not be 
+   // supported in every browser.  It worked for me in Chrome.
+   var extra = !col ? '' : '\ncolumn: ' + col;
+   extra += !error ? '' : '\nerror: ' + error;
+
+   // You can view the information in an alert to see things working like this:
+   alert("scratch2online: globalExceptionHandler: Error: " + msg + "\nurl: " + url + "\nline: " + line + extra);
+
+   // TODO: Report this error via ajax so you can keep track
+   //       of what pages have JS issues
+
+   var suppressErrorAlert = true;
+   // If you return true, then error alerts (like in older versions of 
+   // Internet Explorer) will be suppressed.
+   return suppressErrorAlert;
+};
